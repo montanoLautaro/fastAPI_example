641ba3968fcf9d7863a9a5d7fd3d09f013d317bf
Montaño Heredia Lautaro Nahuel <heredialautaro@gmail.com>
2023-06-26 22:21:44 -0300

    despliegue en space deta

--- fastAPI/database/client.py
+++ fastAPI/database/client.py
@@ -1,3 +1,9 @@
 from pymongo import MongoClient
 
-db_client = MongoClient().local
+# Base de datos local
+# db_client = MongoClient().local
+
+
+# Base de datos remota para python 3.11 or later
+db_client = MongoClient(
+    "mongodb+srv://test:test@cluster0.qsxsbmr.mongodb.net/?retryWrites=true&w=majority").test
